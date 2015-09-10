3f7ef0c26f2b7839af0130b7f47b0f7b164307f9
sherdeadlock <sherdeadlock@gmail.com>
2015-09-10 09:07:09 +0800

    0.0.9

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'sherdeadlock:fixed-data-table',
-  version: '0.0.8',
+  version: '0.0.9',
   // Brief, one-line summary of the package.
   summary: 'npm wrapper for https://github.com/facebook/fixed-data-table',
   // URL to the Git repository containing the source code for this package.
